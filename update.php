495e48bfef0be8b1f7694189c48336b2603c3bbc
Felipe Cesar Sepulvida Rampazzo <felipe.cesar@totvs.com.br>
2021-05-03 14:15:03 -0300

    Documentando Update

--- update.php
+++ update.php
@@ -1,16 +1,20 @@
 <?php
 
-    $idade = 18;
-    $id = 5;
+    IDADE = 18;
+    ID = 5;
 
     try{
 
+        # Inicia a Conexão com o Banco de Dados
         $conn = new PDO('mysql:host=localhost;dbname=teste;', 'root', '');
 
+        # Cria a Query SQL com a criação de duas variáveis -> :idade / :id
         $stmt = $conn->prepare("UPDATE funcionario SET idade = :idade WHERE id = :id");
+
+        # Executa a Query SQL passando uma lista preenchendo as variáveis
         $stmt->execute(array(
-            'idade' => $idade,
-            'id' => $id
+            'idade' => IDADE,
+            'id' => ID
         ));
 
     } catch(Exception $e){
