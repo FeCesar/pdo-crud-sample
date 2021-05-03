76c5ead1bcd41920b341d1a5b3d9de31c9990463
Felipe Cesar Sepulvida Rampazzo <felipe.cesar@totvs.com.br>
2021-05-03 14:05:00 -0300

    Documentando o código de create

--- create.php
+++ create.php
@@ -2,13 +2,17 @@
 
     try{
 
+        # Inicia a Conexão
         $conn = new PDO('mysql:host=localhost;dbname=teste;', 'root', '');
 
+        # Cria a Query SQL com a criação de variáveis -> :nome / :idade / :email
         $stmt = $conn->prepare("INSERT INTO funcionario(nome, idade, email) VALUES(:nome, :idade, :email)");
+
+        # Executa a Query SQL passando uma lista preenchendo as variáveis
         $stmt->execute(array(
             'nome' => "Felipe Cesar Sepulvida Rampazzo",
-            'idade' => 17,
-            'email' => "ff.cc.ss.rr@hotmail.com"
+            'idade' => 18,
+            'email' => "ff.cc.ss.rr@gmail.com"
         ));
 
     } catch(PDOException $e){
