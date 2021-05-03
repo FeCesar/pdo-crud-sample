f4b809d1bd731d8867ae64a4f596d2f9647541f8
Felipe Cesar Sepulvida Rampazzo <felipe.cesar@totvs.com.br>
2021-05-03 14:12:55 -0300

    Documentação do Select

--- select.php
+++ select.php
@@ -1,11 +1,19 @@
 <?php
 
     try{
+
+        # Inicia a Conexão Com o Banco de Dados
         $conn = new PDO('mysql:host=localhost;dbname=teste', 'root', '');
+
+        # Cria a Query SQL
         $stmt = $conn->query("SELECT * FROM funcionario");
         
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            echo "ID: {$row['id']}<br> NOME: {$row['nome']}<br> IDADE: {$row['idade']}<br> E-MAIL: {$row['email']}<br><br>";
+        # Cria um looping mostrando os dados do banco até o fim dos resgistros
+        while($funcionario = $stmt->fetch(PDO::FETCH_ASSOC)){
+            echo "ID: $funcionario['id'] <br>";
+            echo "NOME: $funcionario['nome'] <br>"; 
+            echo "IDADE: $funcionario['idade'] <br>";
+            echo "E-MAIL: $funcionario['email'] <br>";
         }
 
     } catch(PDOException $e){
